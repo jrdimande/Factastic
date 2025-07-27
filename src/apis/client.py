f023115b2fbd4fc6322ff4d8413b633f4d61176b
jrdimande <timoteojrdimande@gmail.com>
2025-07-27 19:11:33 +0200

    feat(client):return resource not found

--- src/apis/client.py
+++ src/apis/client.py
@@ -3,13 +3,16 @@ from src.apis.constants import URL
 
 def generate_random_facts():
     # Get a random fact from the API and return the text
-    response = requests.get(URL)
+    try:
+        response = requests.get(URL)
 
-    if response.ok:
-        fact_data = response.json()
-        fact = fact_data["text"]
-        return fact
-    return "No interner connection"
+        if response.ok:
+            fact_data = response.json()
+            fact = fact_data["text"]
+            return fact
+        return "Resource not  found"
+    except requests.exceptions.ConnectionError:
+        return "No internet connection"
 
 
 
